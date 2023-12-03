be653459ad2d77529f44b1b20eccc1c7497836d2
Vish0111 <it21315282@my.sliit.lk>
2023-12-03 20:39:59 +0530

    favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech , Works, StarsCanvas} from './components';
 
-const App =() => {
+const App = () => {
   return (
     <BrowserRouter>
    <div className="relative z-0 bg-primary">
