5f612ec6bcca43ded0af2ee34f43abe4bea6ff6b
Vish0111 <it21315282@my.sliit.lk>
2023-12-20 23:36:07 +0530

    auto rotation added

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -66,6 +66,8 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          autoRotate
+          autoRotateSpeed={0.5} // Adjust the speed as needed
         />
         <Computers isMobile={isMobile} />
       </Suspense>
