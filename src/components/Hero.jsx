ce4e76cfdef7970c8e859aab87c4db1459e10c51
Vish0111 <it21315282@my.sliit.lk>
2023-12-20 23:04:35 +0530

    bug fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -124,7 +124,9 @@ const Hero = () => {
 
   return (
     <section className={`relative w-full h-screen mx-auto`}>
+
       <div id="particles-js" className={`${styles.particlesContainer} absolute inset-0`} />
+      
        <div
         className={`${styles.paddingX} absolute inset-0 top-[120px]  max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
