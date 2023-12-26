ad22565905907aa7db9205968c55eded1ebe78ff
Vish0111 <it21315282@my.sliit.lk>
2023-12-26 21:05:41 +0530

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech , Works, StarsCanvas} from './components';
+import {Home, About, Contact, Experience, Feedbacks, Hero, Navbar, Tech , Works, StarsCanvas} from './components';
 
 const App = () => {
   return (
@@ -8,6 +8,7 @@ const App = () => {
    <div className="relative z-0 bg-primary">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
+        <Home />
         <Hero />
       </div>
       <About />
