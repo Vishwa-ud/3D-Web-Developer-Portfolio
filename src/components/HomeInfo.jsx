5d29eb54cf41ae466e1e1eecce572615b31625c8
Vish0111 <it21315282@my.sliit.lk>
2023-12-29 21:34:06 +0530

    Bug Fixed

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,53 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import {arrow} from "../assets/icons"
+import { navLinks } from "../constants";
+import { arrow } from "../assets/icons";
 
-const InfoBox = ({text , link , btnText}) => (
+const InfoBox = ({ text, linkId, btnText }) => {
+  const linkObject = navLinks.find((link) => link.id === linkId);
+
+  return (
     <div className="info-box">
-        <p className="font-medium sm:text-xl text-center">{text}</p>
-        <Link to = {link} className="neo-brutalism-white neo-btn">
-            {btnText}
-            <img src={arrow} className='w-4 h-4 object-contain' />
-        </Link>
+      <p className="font-medium sm:text-xl text-center">{text}</p>
+      <a href={linkObject ? `#${linkObject.id}` : "/"} className="neo-brutalism-white neo-btn">
+        {btnText}
+        <img src={arrow} className="w-4 h-4 object-contain" alt="arrow" />
+      </a>
     </div>
-)
-
-
-const renderContent = {
-    1: (
-        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-            Hi, I am<span className="font-semibold">Vishwa</span>
-            👋
-        <br />
-        A Software Engineer from Sri Lanka
-        </h1>
-    ),
-    2: (
-        <InfoBox 
-        text ="Undergatuadte Software Engineer at SLIIT Faculty of Computing"
-        link ="/about"
-        btnText ="Learn more about me"
-        />
-    ),
-    3: (
-        <InfoBox 
-        text ="Led multiple projects to success over the years. Curious about the impact?"
-        link ="/works"
-        btnText ="Visit my portfolio"
-        />
-    ),
-    4: (
-        <InfoBox 
-        text ="Need a project done or looking for a dev? I'm just a few keystrokes away"
-        link ="/contact"
-        btnText ="Let's talk"
-        />
-    )
-} 
-
+  );
+};
 
 const HomeInfo = ({ currentStage }) => {
-    return renderContent [currentStage] || null;
-    
-}
+  const renderContent = {
+    1: (
+      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+        Hi, I am<span className="font-semibold">Vishwa</span> 👋
+        <br />
+        A Software Engineer from Sri Lanka
+      </h1>
+    ),
+    2: (
+      <InfoBox
+        text="Undergraduate Software Engineer at SLIIT Faculty of Computing"
+        linkId="about"
+        btnText="Learn more about me"
+      />
+    ),
+    3: (
+      <InfoBox
+        text="Led multiple projects to success over the years. Curious about the impact?"
+        linkId="work"
+        btnText="Visit my portfolio"
+      />
+    ),
+    4: (
+      <InfoBox
+        text="Need a project done or looking for a dev? I'm just a few keystrokes away"
+        linkId="contact"
+        btnText="Let's talk"
+      />
+    ),
+  };
+
+  return <div>{renderContent[currentStage]}</div>;
+};
+
 export default HomeInfo;
