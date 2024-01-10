a2fe41b3b221d84ba975d1100b85c220051100d0
Vish0111 <it21315282@my.sliit.lk>
2024-01-11 01:24:40 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+
+import { socialLinks } from "../constants";
 
 const Footer = () => {
   return (
@@ -8,6 +11,18 @@ const Footer = () => {
         <p>
           © 2023 <strong>Vishwa Udayntha</strong>. All rights reserved.
         </p>
+
+        <div className='flex gap-3 justify-center items-center'>
+          {socialLinks.map((link) => (
+            <Link key={link.name} to={link.link} target='_blank'>
+              <img
+                src={link.iconUrl}
+                alt={link.name}
+                className='w-6 h-6 object-contain'
+              />
+            </Link>
+          ))}
+        </div>
 
         
       </div>
