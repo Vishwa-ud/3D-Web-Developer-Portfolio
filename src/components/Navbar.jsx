4f321120e66c6b7168a4ed1a9d6225f93217b0cd
Vish0111 <it21315282@my.sliit.lk>
2024-01-14 00:02:02 +0530

    bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src="/logo.png" alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>{/*flex*/}
             Vishwa &nbsp;<span className='sm:block hidden'> | 3D Portfolio</span>
           </p>
